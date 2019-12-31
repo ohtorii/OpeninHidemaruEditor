a9b7d5e0ca403c115c827967fc77ba2876cd15c5
ohtorii <ikeuchi.h9@gmail.com>
2019-12-31 21:43:08 +0900

    Initial commit

---
+++ OpeninHidemaruEditor/settings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Microsoft.VisualStudio.Shell;
+
+
+namespace OpeninHidemaruEditor
+{
+	public class Settings : DialogPage
+	{
+		[Category("General")]
+		[DisplayName("Install path")]
+		[Description("The absolute path to the \"hidemaru.exe\" file.")]
+		public string FolderPath { get; set; }
+
+		public override void LoadSettingsFromStorage()
+		{
+			base.LoadSettingsFromStorage();
+
+			if (!string.IsNullOrEmpty(this.FolderPath))
+			{
+				return;
+			}
+
+			this.FolderPath = FindNotepadPlusPlus();
+		}
+
+		private static string FindNotepadPlusPlus()
+		{
+			return "ダミー";
+
+			var directoryInfo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles));
+			if (directoryInfo.Parent == null)
+			{
+				return null;
+			}
+
+			foreach (var directory in directoryInfo.Parent.GetDirectories(directoryInfo.Name.Replace(" (x86)", string.Empty) + "*"))
+			{
+				foreach (var fileSystemInfo in directory.GetDirectories("Notepad++").Reverse())
+				{
+					var path = Path.Combine(fileSystemInfo.FullName, "notepad++.exe");
+					if (File.Exists(path))
+					{
+						return path;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+}
