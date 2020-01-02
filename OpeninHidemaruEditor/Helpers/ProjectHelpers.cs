26ada6300fc7fc2567951da0ad250b22183af5bf
ohtorii <ikeuchi.h9@gmail.com>
2020-01-02 13:54:31 +0900

    Create ProjectHelpers.cs

---
+++ OpeninHidemaruEditor/Helpers/ProjectHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace OpeninHidemaruEditor.Helpers
+{
+	internal static class ProjectHelpers
+	{
+		public static string GetSelectedPath(DTE2 dte)
+		{
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+			foreach (UIHierarchyItem selectedItem in (Array)dte.ToolWindows.SolutionExplorer.SelectedItems)
+			{
+				switch (selectedItem.Object)
+				{
+					case ProjectItem projectItem:
+						return projectItem.FileNames[1];
+					case Project project:
+						return project.FullName;
+					case Solution solution:
+						return solution.FullName;
+				}
+			}
+			return null;
+		}
+	}
+}
